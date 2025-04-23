f058800757ef76516e2299c156ca4726139883f1
moizarajdev <moizaraj@gmail.com>
2025-04-23 13:45:29 -0500

    Changed the script to create a scheduled event on boot to copy folder over rather than attempting to do it during deployment.

--- script.ps1
+++ script.ps1
@@ -5,12 +5,11 @@ $vdotDir = "$env:TEMP\Virtual-Desktop-Optimization-Tool-main"
 Invoke-WebRequest -Uri $vdotZipUrl -OutFile $vdotZipFile -ErrorAction Stop
 Expand-Archive -LiteralPath $vdotZipFile -DestinationPath $env:TEMP -Force -ErrorAction Stop
 
-& "$vdotDir\Windows_VDOT.ps1" -AcceptEULA -Optimizations All -AdvancedOptimizations RemoveOneDrive, Edge -Verbose
+& "$vdotDir\Windows_VDOT.ps1" -AcceptEULA -Optimizations All -AdvancedOptimizations All -Verbose
 
-$srcPath = '\\usmrimas\Rimas_NTP\PSBinaries\*'
-$destPath = 'C:\RIMAS_NTP'
-
-if (-not (Test-Path $destPath)) { New-Item -Path $destPath -ItemType Directory -Force | Out-Null }
-Copy-Item -Path $srcPath -Destination $destPath -Recurse -Force -ErrorAction Stop
+$taskName = 'CopyRimasNTPData'
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument '-NoProfile -WindowStyle Hidden -Command "if(-not(Test-Path ''C:\RIMAS_NTP'')){New-Item -Path ''C:\RIMAS_NTP'' -ItemType Directory|Out-Null};robocopy ''\\usmrimas\Rimas_NTP\PSBinaries\Data'' ''C:\RIMAS_NTP'' /MIR /R:2 /W:5"'
+$trigger = New-ScheduledTaskTrigger -AtStartup -Delay '00:00:10'
+Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -RunLevel Highest -User 'SYSTEM' -Force
 
 Restart-Computer -Force
