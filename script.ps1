433ebb0243bbd8c26a82e2cd69f00fad536e5591
moizarajdev <moizaraj@gmail.com>
2025-04-24 12:01:22 -0500

    Replaced the delay switch as it's not supported in Powershell 5.1

--- script.ps1
+++ script.ps1
@@ -5,11 +5,14 @@ $vdotDir = "$env:TEMP\Virtual-Desktop-Optimization-Tool-main"
 Invoke-WebRequest -Uri $vdotZipUrl -OutFile $vdotZipFile -ErrorAction Stop
 Expand-Archive -LiteralPath $vdotZipFile -DestinationPath $env:TEMP -Force -ErrorAction Stop
 
-& "$vdotDir\Windows_VDOT.ps1" -AcceptEULA -Optimizations All -AdvancedOptimizations All -Verbose
+& "$vdotDir\Windows_VDOT.ps1" -AcceptEULA -Optimizations All -AdvancedOptimizations RemoveOneDrive, Edge -Verbose
 
 $taskName = 'CopyRimasNTPData'
 $action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument '-NoProfile -WindowStyle Hidden -Command "if(-not(Test-Path ''C:\RIMAS_NTP'')){New-Item -Path ''C:\RIMAS_NTP'' -ItemType Directory|Out-Null};robocopy ''\\usmrimas\Rimas_NTP\PSBinaries\Data'' ''C:\RIMAS_NTP'' /MIR /R:2 /W:5"'
-$trigger = New-ScheduledTaskTrigger -AtStartup -Delay '00:00:10'
-Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -RunLevel Highest -User 'SYSTEM' -Force
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger.Delay = New-TimeSpan -Seconds 30
+$principal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -RunLevel Highest
+
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Principal $principal -Force
 
 Restart-Computer -Force
