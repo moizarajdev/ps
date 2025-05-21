da368e6c0d498dd92327949101b8fd63cdfaf767
Moiz Araj <maraj@unitedscrap.com>
2025-05-21 13:29:02 -0500

    Converted to Bicep - the full template works besides the VDOT package. I'm not sure why it works through the portal and not the template, we need to figure it out.

---
+++ azuredeploy.bicep
@@ -0,0 +1,140 @@
+targetScope = 'resourceGroup'
+
+// Parameters
+param location string
+param hostPoolName string
+param registrationInfoToken string
+param registrationInfoExpirationTime string
+param sessionHostNames array
+param vmSize string
+param adminUsername string
+@secure()
+param adminPassword string
+param imagePublisher string
+param imageOffer string
+param imageSku string
+param imageVersion string = 'latest'
+param vnetResourceGroup string
+param vnetName string
+param subnetName string
+param nsgId string
+param domainToJoin string
+param ouPath string = ''
+@secure()
+param domainJoinUsername string
+@secure()
+param domainJoinPassword string
+param scriptUri string
+param osDiskType string = 'Standard_LRS'
+param diskSizeGB int = 128
+
+// Variables
+var subnetResourceId = resourceId(vnetResourceGroup, 'Microsoft.Network/virtualNetworks/subnets', vnetName, subnetName)
+
+// Update registration token on Host Pool
+resource hostPool 'Microsoft.DesktopVirtualization/hostPools@2024-11-01-preview' = {
+  name: hostPoolName
+  location: location
+  properties: {
+    hostPoolType: 'Pooled'
+    loadBalancerType: 'BreadthFirst'
+    preferredAppGroupType: 'Desktop'
+    friendlyName: hostPoolName
+    registrationInfo: {
+      registrationTokenOperation: 'Update'
+      token: registrationInfoToken
+      expirationTime: registrationInfoExpirationTime
+    }
+  }
+}
+
+// Network Interfaces
+resource nics 'Microsoft.Network/networkInterfaces@2024-05-01' = [for (name, i) in sessionHostNames: {
+  name: '${name}-nic'
+  location: location
+  dependsOn: [ hostPool ]
+  properties: {
+    ipConfigurations: [
+      {
+        name: 'ipconfig1'
+        properties: {
+          subnet: { id: subnetResourceId }
+          privateIPAllocationMethod: 'Dynamic'
+        }
+      }
+    ]
+    networkSecurityGroup: { id: nsgId }
+  }
+}]
+
+// Virtual Machines
+resource vms 'Microsoft.Compute/virtualMachines@2024-11-01' = [for (name, i) in sessionHostNames: {
+  name: name
+  location: location
+  dependsOn: [ nics[i], hostPool ]
+  identity: { type: 'SystemAssigned' }
+  properties: {
+    hardwareProfile: { vmSize: vmSize }
+    storageProfile: {
+      imageReference: {
+        publisher: imagePublisher
+        offer: imageOffer
+        sku: imageSku
+        version: imageVersion
+      }
+      osDisk: {
+        createOption: 'FromImage'
+        managedDisk: { storageAccountType: osDiskType }
+        diskSizeGB: diskSizeGB
+      }
+    }
+    osProfile: {
+      computerName: name
+      adminUsername: adminUsername
+      adminPassword: adminPassword
+      windowsConfiguration: { enableAutomaticUpdates: true }
+    }
+    networkProfile: { networkInterfaces: [ { id: nics[i].id } ] }
+    diagnosticsProfile: { bootDiagnostics: { enabled: true } }
+  }
+}]
+
+// Domain Join Extension (top-level loop)
+resource domainJoinExt 'Microsoft.Compute/virtualMachines/extensions@2024-11-01' = [for (name, i) in sessionHostNames: {
+  name: 'joindomain-${name}'
+  parent: vms[i]
+  location: location
+  dependsOn: [ vms[i] ]
+  properties: {
+    publisher: 'Microsoft.Compute'
+    type: 'JsonADDomainExtension'
+    typeHandlerVersion: '1.3'
+    autoUpgradeMinorVersion: true
+    settings: {
+      Name: domainToJoin
+      OUPath: ouPath
+      User: domainJoinUsername
+      Restart: 'true'
+    }
+    protectedSettings: { Password: domainJoinPassword }
+  }
+}]
+
+// Custom Script Extension
+resource customScriptExt 'Microsoft.Compute/virtualMachines/extensions@2024-11-01' = [for (name, i) in sessionHostNames: {
+  name: 'CustomScript-${name}'
+  parent: vms[i]
+  location: location
+  dependsOn: [ domainJoinExt[i] ]
+  properties: {
+    publisher: 'Microsoft.Compute'
+    type: 'CustomScriptExtension'
+    typeHandlerVersion: '2.0'
+    autoUpgradeMinorVersion: true
+    settings: { fileUris: [ scriptUri ] }
+    protectedSettings: { commandToExecute: 'powershell -ExecutionPolicy Unrestricted -File ${last(split(scriptUri, '/'))} -Optimizations All -AcceptEULA' }
+  }
+}]
+
+// Output
+output sessionHostNames array = sessionHostNames
--- deploy.ps1
+++ deploy.ps1
@@ -1,47 +1,56 @@
 Connect-AzAccount
-Set-AzContext -SubscriptionId "56e282ac-7d87-46ad-900a-4c0ed6912002"
+Set-AzContext -SubscriptionId '56e282ac-7d87-46ad-900a-4c0ed6912002'
 
-$prefix = "win11test"
+$prefix = 'win11test'
 $newCount = 3
 
-$existingIdx = Get-AzVM -ResourceGroupName "RG-AVD-Dev" |
-Where-Object Name -match "^$prefix-(\d+)$" |
-ForEach-Object { [int]$Matches[1] }
+$existingIdx = Get-AzVM -ResourceGroupName 'RG-AVD-Dev' |
+  ForEach-Object {
+    if ($_.Name -match "^$prefix-(\d+)$") { [int]$Matches[1] }
+  }
+
 $maxIdx = if ($existingIdx) { ($existingIdx | Measure-Object -Maximum).Maximum } else { -1 }
 
 $sessionHostNames = 1..$newCount | ForEach-Object { "$prefix-$($maxIdx + $_)" }
 
-$expireUTC = (Get-Date).ToUniversalTime().AddHours(4).ToString("yyyy-MM-ddTHH:mm:ssZ")
-$regInfo = New-AzWvdRegistrationInfo -ResourceGroupName "RG-AVD-Dev" -HostPoolName "HP-AVDDev" -ExpirationTime $expireUTC
+$expirationTime = (Get-Date).AddHours(4).ToUniversalTime()
+$regInfo = New-AzWvdRegistrationInfo `
+  -ResourceGroupName 'RG-AVD-Dev' `
+  -HostPoolName 'HP-AVDDev' `
+  -ExpirationTime $expirationTime
+
+$expirationString = $regInfo.ExpirationTime.ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
 
 $secrets = @{
-    adminPassword      = (Get-AzKeyVaultSecret -VaultName "KV-DomainVault" -Name "VMLocalAdminPassword").SecretValue
-    domainJoinUsername = (Get-AzKeyVaultSecret -VaultName "KV-DomainVault" -Name "DomainEnrollUsername").SecretValue
-    domainJoinPassword = (Get-AzKeyVaultSecret -VaultName "KV-DomainVault" -Name "DomainEnrollPassword").SecretValue
+  adminPassword      = (Get-AzKeyVaultSecret -VaultName 'KV-DomainVault' -Name 'VMLocalAdminPassword').SecretValue
+  domainJoinUsername = (Get-AzKeyVaultSecret -VaultName 'KV-DomainVault' -Name 'DomainEnrollUsername').SecretValue
+  domainJoinPassword = (Get-AzKeyVaultSecret -VaultName 'KV-DomainVault' -Name 'DomainEnrollPassword').SecretValue
 }
 
+$imageVersion = 'latest'
+
 New-AzResourceGroupDeployment `
-    -Name                          "avd-names-array" `
-    -ResourceGroupName             "RG-AVD-Dev" `
-    -TemplateFile                  "azuredeploy.json" `
-    -location                      "northcentralus" `
-    -hostPoolName                  "HP-AVDDev" `
-    -sessionHostNames              $sessionHostNames `
-    -vmSize                        "Standard_D4as_v5" `
-    -imagePublisher                "MicrosoftWindowsDesktop" `
-    -imageOffer                    "office-365" `
-    -imageSku                      "win11-24h2-avd-m365" `
-    -imageVersion                  "latest" `
-    -vnetResourceGroup             "RG-AVD-Dev" `
-    -vnetName                      "VNET-TestAvdNetwork" `
-    -subnetName                    "default" `
-    -nsgId                         "/subscriptions/56e282ac-7d87-46ad-900a-4c0ed6912002/resourceGroups/RG-AVD-Dev/providers/Microsoft.Network/networkSecurityGroups/NSG-VNET-TestAVDNetwork-default" `
-    -adminUsername                 "usmavdadmin" `
-    -adminPassword                 $secrets.adminPassword `
-    -domainToJoin                  "usm1.local" `
-    -ouPath                        "OU=VMs,OU=USM_New_AVD,DC=usm1,DC=local" `
-    -domainJoinUsername            $secrets.domainJoinUsername `
-    -domainJoinPassword            $secrets.domainJoinPassword `
-    -registrationInfoToken         $regInfo.Token `
-    -registrationInfoExpirationTime $expireUTC `
-    -scriptUri                     "https://raw.githubusercontent.com/moizarajdev/ps/main/script.ps1"
+  -Name 'avd-bicep-deploy' `
+  -ResourceGroupName 'RG-AVD-Dev' `
+  -TemplateFile 'azuredeploy.bicep' `
+  -location 'northcentralus' `
+  -hostPoolName 'HP-AVDDev' `
+  -registrationInfoToken $regInfo.Token `
+  -registrationInfoExpirationTime $expirationString `
+  -sessionHostNames $sessionHostNames `
+  -vmSize 'Standard_D4as_v5' `
+  -imagePublisher 'MicrosoftWindowsDesktop' `
+  -imageOffer 'office-365' `
+  -imageSku 'win11-24h2-avd-m365' `
+  -imageVersion $imageVersion `
+  -vnetResourceGroup 'RG-AVD-Dev' `
+  -vnetName 'VNET-TestAvdNetwork' `
+  -subnetName 'default' `
+  -nsgId '/subscriptions/56e282ac-7d87-46ad-900a-4c0ed6912002/resourceGroups/RG-AVD-Dev/providers/Microsoft.Network/networkSecurityGroups/NSG-VNET-TestAVDNetwork-default' `
+  -adminUsername 'usmavdadmin' `
+  -adminPassword $secrets.adminPassword `
+  -domainToJoin 'usm1.local' `
+  -ouPath 'OU=VMs,OU=USM_New_AVD,DC=usm1,DC=local' `
+  -domainJoinUsername $secrets.domainJoinUsername `
+  -domainJoinPassword $secrets.domainJoinPassword `
+  -scriptUri 'https://raw.githubusercontent.com/moizarajdev/ps/main/script.ps1'
